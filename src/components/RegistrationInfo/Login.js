928ce42307c8f1d45c69dd71219e0d5a628dabac
unknown <sajeda1605akter@gmail.com>
2022-10-24 08:52:59 -0700

    login page complete

--- src/components/RegistrationInfo/Login.js
+++ src/components/RegistrationInfo/Login.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import google from '../../assents/google.png'
+import github from '../../assents/github.png'
+import facebook from '../../assents/facebook.png'
 
 const Login = () => {
     return (
-        <div>
-            
-        </div>
+        <Form className='  border border-light m-auto shadow-lg p-2 ' style={{maxWidth:'480px'}}>
+            <h3 className='text-center mb-5 pt-4'>Please Login! </h3>
+        <Form.Group className="mb-3 " controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+         
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+       <div className='d-flex justify-content-between mx-3'>
+       <Link to='/signup'><p>Create a new acount </p>
+       </Link>
+       <Link><p>forget password</p></Link>
+       </div>
+       <p className='text-center fs-4'>or</p>
+       <hr className='w-50 m-auto'/>
+       <div className='d-flex justify-content-evenly  align-items-center mt-5'>
+       <p>
+        <img src={google} alt="" style={{height:'40px'}}/>
+       Google</p>
+       <p>
+        <img src={github} style={{height:'40px'}} alt="" />
+        Github</p>
+       <p>
+        <img src={facebook} style={{height:'40px'}} alt="" />
+        Facebook</p>
+
+       </div>
+        <Button variant="primary" type="submit" className=' w-50 p-2 mt-5' style={{marginLeft:'120px'}}>
+          Login
+        </Button>
+      </Form>
     );
 };
 
--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -3,13 +3,16 @@ import Main from "../../Main/Main";
 import Blog from "../Blog/Blog";
 import Courses from "../Courses/Courses";
 import FAQ from "../FAQ/FAQ";
+import Login from "../RegistrationInfo/Login";
+import Signup from "../RegistrationInfo/Signup";
 
 export const router=createBrowserRouter([
     {path:'/',element:<Main/>,children:[
         {path:'/courses',element:<Courses/>},
         {path:'/faq',element:<FAQ/>},
         {path:'/blog',element:<Blog/>},
-        
+        {path:'/login',element:<Login/>},
+        {path:'/signup',element:<Signup/>}
     ]
 }
 ])
