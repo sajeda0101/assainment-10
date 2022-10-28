ad9add216ed9a871f29e2f05f9b262fe22d29f18
unknown <sajeda1605akter@gmail.com>
2022-10-28 10:28:20 -0700

    change again

--- src/components/routes/PrivateRoute/PrivateRoute.js
+++ src/components/routes/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { AuthContext } from "../UserContext/UserContext";
+import { AuthContext } from "../../UserContext/UserContext";
+
 
 const PrivateRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
